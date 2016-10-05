1dfc7f94e863e84d23dcee5e6839285a14149d9c
rajithst <rajiththennakoonshady@gmail.com>
2016-10-05 13:46:24 +0530

    add admin dashboard

---
+++ application/controllers/admin/Dashboard.php
@@ -0,0 +1,23 @@
+<?php
+class Dashboard extends Admin_Controller{
+
+
+    /**
+     * Dashboard constructor.
+     */
+    public function __construct(){
+
+        parent::__construct();
+    }
+
+    /**
+     * @return object
+     */
+    public function index()
+    {
+         $this->load->view('admin/index');
+    }
+
+
+
+}
---
+++ application/views/admin/components/pagetail.php
@@ -0,0 +1,9 @@
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="<?php echo base_url('assets/plugins/jquery/jquery.js'); ?>"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/plugins/fastclick/fastclick.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/admin/custom.js'); ?>"></script>
+</body>
+</html>
